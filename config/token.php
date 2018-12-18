4dbe67ac999fbc78ad6025205a5aa71a4d8d2f70
RodrigoDornelles <rodrigo.barbosa.dornelles@gmail.com>
2018-12-18 21:36:08 -0200

    token config

---
+++ config/token.php
@@ -0,0 +1,13 @@
+<?php return [
+
+    /* how to create a discord bot (https://discordapp.com/developers/applications)
+    Go to their Developer site and create an application.  
+    Simply press on the "New Application" button. 
+    Give the application a name, this should be the name you want your bot on Discord to have.
+    Create the actual Discord bot user by clicking on "Create a Bot User".
+    You can now click on the link beside Token under Bot User to see your bot token.
+    */
+  
+    'token' => 'your bot token id here'
+  
+];
