fdb42cf1e01bb69f7a19f72fd309c81c7f0a42cb
RodrigoDornelles <rodrigodornelles@github.com>
2018-12-19 00:14:50 +0000

    added philosophers

---
+++ src/philosophers.php
@@ -0,0 +1,27 @@
+<?php return [
+//---------------------------------------------
+	'arthur schopenhauer'=>'Arthur Schopenhauer',
+	'schopenhauer'=>'Arthur Schopenhauer',
+	'schopen'=>'Arthur Schopenhauer',
+	'arthur'=>'Arthur Schopenhauer',
+	'artur'=>'Arthur Schopenhauer',
+	'as'=>'Arthur Schopenhauer',
+//---------------------------------------------
+	'carl sagan'=>'Carl Sagan',
+	'sagan'=>'Carl Sagan',
+	'carl'=>'Carl Sagan',
+	'sg'=>'Carl Sagan',
+//---------------------------------------------
+	'friedrich nietzsche'=>'Friedrich Nietzsche',
+	'nietzsche'=>'Friedrich Nietzsche',
+	'nietz'=>'Friedrich Nietzsche',
+	'niet'=>'Friedrich Nietzsche',
+	'fn'=>'Friedrich Nietzsche',
+//---------------------------------------------
+	'papai Noel'=>'Karl Marx',
+	'karl marx'=>'Karl Marx',
+	'marx'=>'Karl Marx',
+	'karl'=>'Karl Marx',	
+	'km'=>'Karl Marx'
+//---------------------------------------------
+];
